7a26c86d77b4c07c4b409caabd6d6492b5805fb1
khansalsabila <lathifahks1702@gmail.com>
2022-12-04 20:44:35 +0700

    second commit

--- src/main/java/wc/s/ProjectRestFull/Product.java
+++ src/main/java/wc/s/ProjectRestFull/Product.java
@@ -9,5 +9,23 @@ package wc.s.ProjectRestFull;
  * @author USER DJOGJA
  */
 class Product {
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+   private String id;
+   private String name;
     
 }
