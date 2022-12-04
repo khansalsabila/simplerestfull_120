effc98883b6ceb13b1bc0b1c4a271917baccd708
khansalsabila <lathifahks1702@gmail.com>
2022-12-04 20:42:12 +0700

    first commit

---
+++ src/main/java/wc/s/ProjectRestFull/ProjectRestFullApplication.java
@@ -0,0 +1,13 @@
+package wc.s.ProjectRestFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRestFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wc/s/ProjectRestFull/ProjectRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package wc.s.ProjectRestFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
